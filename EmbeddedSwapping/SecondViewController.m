7f1fd7bba6871029606fb32a06d4b97662b12fac
Michael Luton <mluton@gmail.com>
2013-06-24 22:46:49 -0700

    enable user interaction on subviews for SecondViewController

--- EmbeddedSwapping/SecondViewController.m
+++ EmbeddedSwapping/SecondViewController.m
@@ -27,6 +27,12 @@
 {
     [super viewDidLoad];
 	NSLog(@"SecondViewController - viewDidLoad");
+    
+    // Make it so any subviews can respond to user interaction. For example,
+    // scroll views responding to scroll events. It's not enough for the
+    // subview to have userInteractionEnabled set to true. It needs be set in
+    // the container view as well. --mluton (2013-06-24)
+    self.view.userInteractionEnabled = YES;
 }
 
 - (void)didReceiveMemoryWarning
