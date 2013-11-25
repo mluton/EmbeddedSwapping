2ca5f8c303edabe2562cf41a0131019f377e2973
smolin_in <smolin_in@tgt72.ru>
2013-11-25 09:42:12 +0600

    add autoresize mask

--- EmbeddedSwapping/ContainerViewController.m
+++ EmbeddedSwapping/ContainerViewController.m
@@ -57,8 +57,10 @@
             // If this is the very first time we're loading this we need to do
             // an initial load and not a swap.
             [self addChildViewController:segue.destinationViewController];
-            ((UIViewController *)segue.destinationViewController).view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-            [self.view addSubview:((UIViewController *)segue.destinationViewController).view];
+            UIView* destView = ((UIViewController *)segue.destinationViewController).view;
+            destView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+            destView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+            [self.view addSubview:destView];
             [segue.destinationViewController didMoveToParentViewController:self];
         }
     }
