e33b2fc37f598fd0586d7c2aeababb0a9e532d25
Emil <heitara@gmail.com>
2016-09-11 23:07:35 +0300

    Swift implementation
    Swift implementation of ContainerViewController.swift - which handles VC switching using Storyboard setup and Seque.

---
+++ EmptySegue.swift
@@ -0,0 +1,17 @@
+//
+//  EmptySegue.swift
+//  OrthodoxCalendar
+//
+//  Created by Emil Atanasov on 9/10/16.
+//  Copyright © 2016 Appose Studio Inc. All rights reserved.
+//
+
+import Foundation
+
+class EmptySegue: UIStoryboardSegue {
+    override func perform() {
+//        let src = self.sourceViewController
+//        let dst = self.destinationViewController
+//        src.navigationController?.pushViewController(dst, animated: true)
+    }
+}
