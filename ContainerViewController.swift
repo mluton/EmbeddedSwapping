4b80128b3ca6e24792498864e745778789440fd6
Sean Soper <sean.soper@gmail.com>
2016-10-07 11:27:08 -0400

    Converted to Swift 3

--- ContainerViewController.swift
+++ ContainerViewController.swift
@@ -6,77 +6,79 @@
 //  Copyright © 2016 Appose Studio Inc. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 //Container View Controller, which swaps between two view controllers
 //based on: http://sandmoose.com/post/35714028270/storyboards-with-custom-container-view-controllers
 //
-class ContainerViewController:UIViewController {
+class ContainerViewController: UIViewController {
 
     static let SegueIdentifierFirst = "embedFirst"
     static let SegueIdentifierSecond = "embedSecond"
     static let SegueIdentifierEmpty = "empty"
 
-    internal var currentSegueIdentifier: String = "empty";
-    
+    internal var currentSegueIdentifier: String = "empty"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if(self.currentSegueIdentifier == ContainerViewController.SegueIdentifierEmpty) {
-            self.currentSegueIdentifier = ContainerViewController.SegueIdentifierFirst;
+
+        if self.currentSegueIdentifier == ContainerViewController.SegueIdentifierEmpty {
+            self.currentSegueIdentifier = ContainerViewController.SegueIdentifierFirst
         }
-        
-        self.performSegueWithIdentifier(self.currentSegueIdentifier, sender: nil)
+
+        self.performSegue(withIdentifier: self.currentSegueIdentifier, sender: nil)
     }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == ContainerViewController.SegueIdentifierFirst {
-            if (self.childViewControllers.count > 0) {
-                self.swapFromViewController(self.childViewControllers[0], toViewController: segue.destinationViewController, offset: 44.0)
-            }
-            else {
-                segue.destinationViewController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
-                self.swapFromViewController(nil,toViewController: segue.destinationViewController,offset: 44.0)
+            if !self.childViewControllers.isEmpty {
+                self.swapFromViewController(fromViewController: self.childViewControllers[0], toViewController: segue.destination, offset: 44.0)
+            } else {
+                segue.destination.view.frame = CGRect(origin: CGPoint.zero, size: self.view.frame.size)
+                self.swapFromViewController(fromViewController: nil, toViewController: segue.destination, offset: 44.0)
             }
         } else if segue.identifier == ContainerViewController.SegueIdentifierSecond {
-            if (self.childViewControllers.count > 0) {
-                self.swapFromViewController(self.childViewControllers[0], toViewController: segue.destinationViewController, offset:44.0)
+            if !self.childViewControllers.isEmpty {
+                self.swapFromViewController(fromViewController: self.childViewControllers[0], toViewController: segue.destination, offset: 44.0)
             } else {
-                segue.destinationViewController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
-                self.swapFromViewController(nil,toViewController: segue.destinationViewController,offset: 44.0)
+                segue.destination.view.frame = CGRect(origin: CGPoint.zero, size: self.view.frame.size)
+                self.swapFromViewController(fromViewController: nil, toViewController: segue.destination, offset: 44.0)
             }
         }
     }
-    
-    
-    func swapFromViewController(fromViewController:UIViewController?, toViewController:UIViewController, offset:CGFloat) -> Void {
-        
-        toViewController.view.frame = CGRectMake(0, 0 + offset, self.view.frame.size.width, self.view.frame.size.height - offset);
-        
-        print("Frame: " , toViewController.view.frame)
-        
-        
+
+    func swapFromViewController(fromViewController: UIViewController?, toViewController: UIViewController, offset: CGFloat) -> Void {
+
+        toViewController.view.frame = CGRect(x: 0, y: 0 + offset, width: self.view.frame.size.width, height: self.view.frame.size.height - offset)
+
+        // DEBUG
+        print("Frame: \(toViewController.view.frame)")
+
         if let fromViewController = fromViewController {
-            fromViewController.willMoveToParentViewController(nil)
+            fromViewController.willMove(toParentViewController: nil)
             addChildViewController(toViewController)
-            
-            transitionFromViewController(fromViewController, toViewController: toViewController, duration: 1.0,
-                                         options: UIViewAnimationOptions.TransitionCrossDissolve, animations: nil) { (finished) in
-                fromViewController.removeFromParentViewController()
-                toViewController.didMoveToParentViewController(self)
+
+            transition(from: fromViewController, to: toViewController, duration: 1.0,
+                       options: UIViewAnimationOptions.transitionCrossDissolve, animations: nil) { (finished) in
+                        fromViewController.removeFromParentViewController()
+                        toViewController.didMove(toParentViewController: self)
             }
         } else {
             addChildViewController(toViewController)
             self.view.addSubview(toViewController.view)
-            toViewController.didMoveToParentViewController(self)
+            toViewController.didMove(toParentViewController: self)
         }
 
     }
 
     func swapViewControllers() -> Void {
-        self.currentSegueIdentifier = (self.currentSegueIdentifier == ContainerViewController.SegueIdentifierFirst) ? ContainerViewController.SegueIdentifierSecond : ContainerViewController.SegueIdentifierFirst;
-        self.performSegueWithIdentifier(self.currentSegueIdentifier, sender: nil)
+        if self.currentSegueIdentifier == ContainerViewController.SegueIdentifierFirst {
+            self.currentSegueIdentifier = ContainerViewController.SegueIdentifierSecond
+        } else {
+            self.currentSegueIdentifier = ContainerViewController.SegueIdentifierFirst
+        }
+
+        self.performSegue(withIdentifier: self.currentSegueIdentifier, sender: nil)
     }
     
 }
